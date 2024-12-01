1dbebd5b0d2d117a314f83e292b6329e62bb1c4e
Duvandoe <andresdoe30@gmail.com>
2024-12-01 17:31:37 -0500

    CSS SERVICIO

--- src/pages/servicio.jsx
+++ src/pages/servicio.jsx
@@ -8,8 +8,22 @@ function servicio() {
         <section className="servicio" id="servicio">
             <div className="servicio-content">
                 <h3>Servicios</h3>
-                <h1>Nuestros servicios</h1>
-                
+                <h4>En ServiTIC, ofrecemos un amplio catálogo de soluciones tecnológicas diseñadas para satisfacer las necesidades de reparación y gestión tecnológica de nuestros clientes. 
+                Nuestro equipo de expertos está comprometido a brindar servicios rápidos, confiables y personalizados. <br></br><br></br></h4>
+                <p><b>Reparación de Dispositivos Electrónicos</b> <br></br> </p>
+                - Computadoras y Laptops:
+                Diagnóstico, reparación de hardware y optimización de software.  <br></br>
+                - Teléfonos Móviles y Tabletas: Soluciones para problemas de pantalla, batería, conectividad y más.  <br></br>
+                - Otros Aparatos Electrónicos: Reparación de dispositivos como impresoras, routers, y accesorios tecnológicos.<br></br>
+
+                <p><b>Gestión y Automatización de Nómina</b> <br></br></p>
+                - Cálculo Automatizado: Procesamos salarios, deducciones y bonificaciones con precisión.<br></br>
+                - Portal de Empleados: Acceso seguro a información de nómina y consultas en tiempo real.<br></br>
+                - Generación de Informes: Informes detallados en formatos PDF y Excel para un análisis rápido y eficiente.
+                <p><b>Soporte Técnico Personalizado</b> <br></br></p>
+                - Atención al Cliente: Asesoramiento y soporte técnico en todas las etapas del proceso. <br></br> 
+                - Actualizaciones y Mantenimiento: Integración con sistemas externos y normativas actualizadas.  <br></br>
+                - Gestión de Reparaciones: Seguimiento en tiempo real del estado de las reparaciones y notificaciones automáticas.
             </div>
         </section>
     </>
